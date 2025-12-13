9dde460e3de84443629af5361c9c8311176ac704
Ricken Diniz <dinizrickenn@gmail.com>
2025-12-13 19:25:11 -0300

    fix: add missing columns in Item model

--- backend/models/item.py
+++ backend/models/item.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Text, Numeric
+from sqlalchemy import Column, Integer, String, Text, Numeric, Date
 from sqlalchemy.orm import relationship
 from app.database.database import Base
+from datetime import datetime
 
 class Item(Base):
     __tablename__ = "Item"
@@ -10,6 +11,10 @@ class Item(Base):
     measure_unity = Column(String, nullable=False)
     amount = Column(Numeric, nullable=False)
     description = Column(Text)
+    price = Column(Numeric, default=0)
+    expiration_date = Column(Date)
+    create_at = Column(Date, default=datetime.utcnow)
+    update_at = Column(Date)
 
     receita_itens = relationship(
         "Receita_Item",
