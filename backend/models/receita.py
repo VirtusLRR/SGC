8b113de04ccd85ea57efd8451854c6fcdc1403d0
Ricken Diniz <dinizrickenn@gmail.com>
2025-12-13 18:38:16 -0300

    feat: add Receita model

---
+++ backend/models/receita.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import relationship
+from app.database.database import Base
+
+class Receita(Base):
+    __tablename__ = "Receita"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    steps = Column(Text, nullable=False)
+    description = Column(Text)
+
+    receita_itens = relationship(
+        "ReceitaItem",
+        back_populates="receita",
+        cascade="all, delete-orphan"
+    )
