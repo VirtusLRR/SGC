fbb96711089fcc9499acf43bb33fd217cb447dc6
Ricken Diniz <dinizrickenn@gmail.com>
2025-12-14 19:44:46 -0300

    feat: FastAPI runner with uvicorn

---
+++ backend/index.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from routes import item_routes
+
+app = FastAPI(
+    title="Minha API",
+    version="1.0.0"
+)
+
+app.include_router(item_routes)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "index:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
