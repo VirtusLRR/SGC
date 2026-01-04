9526bb87f33edc0bfdb68e7f544fcee350b2b374
Ryan <ryansoaresjorvino@gmail.com>
2026-01-04 11:56:01 -0300

    test: update test_models to reflect the new colum in item model

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,27 +1,30 @@
 from models.recipe import Recipe
 from models.item import Item
 from models.recipe_item import RecipeItem
-from tests.conftest import db_session 
+from tests.conftest import db_session
+
 
 def test_insert_item(db_session):
-    
     farinha = Item(
         name="Farinha",
-        measure_unity="g",
+        measure_unity="grama",
         amount=1000,
-        description="Farinha de trigo"
+        description="Farinha de trigo",
+        price=5.00,
+        price_unit="kg"
     )
 
     db_session.add(farinha)
     db_session.commit()
 
     assert farinha.id is not None
-    
+
     filtered_item = db_session.query(Item).filter_by(name="Farinha").first()
     assert filtered_item.name == farinha.name
+    assert filtered_item.price_unit == "kg"
+
 
 def test_insert_recipe(db_session):
-
     recipe = Recipe(
         title="Bolo simples",
         steps="Misture tudo e asse",
@@ -35,14 +38,16 @@ def test_insert_recipe(db_session):
 
     filtered_recipe = db_session.query(Recipe).filter_by(title="Bolo simples").first()
     assert filtered_recipe.title == recipe.title
-    
-def test_insert_recipe_item(db_session):
 
+
+def test_insert_recipe_item(db_session):
     farinha = Item(
         name="Farinha",
-        measure_unity="g",
+        measure_unity="grama",
         amount=1000,
-        description="Farinha de trigo"
+        description="Farinha de trigo",
+        price=5.00,
+        price_unit="kg"
     )
 
     recipe = Recipe(
@@ -67,7 +72,7 @@ def test_insert_recipe_item(db_session):
 
     itens_name = [ri.item.name for ri in recipe.recipe_itens]
     assert "Farinha" in itens_name
-    
+
     amount = {ri.item.name: ri.amount for ri in recipe.recipe_itens}
     assert amount["Farinha"] == 200
 
