76526280d147c7558a0587f781d71d6333253076
Ryan <ryansoaresjorvino@gmail.com>
2026-01-08 21:07:01 -0300

    fix: add chatbotWidget in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { InventoryOverview } from './features/items/views/InventoryOverview'
+import { ChatbotWidget } from './features/chatbot/components/chatbot-widget/ChatbotWidget'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
       <InventoryOverview />
+      <ChatbotWidget />
     </div>
   )
 }
