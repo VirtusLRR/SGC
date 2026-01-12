07e85d940c6db8727adc0b432e99e95f0bee46e0
Ryan <ryansoaresjorvino@gmail.com>
2026-01-11 21:22:49 -0300

    feat: update vite.config.js to comport the system in docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Permite que o servidor seja acessado fora do container (essencial para o Docker)
+    host: '0.0.0.0',
+    // Define a porta para 5173 (deve ser a mesma que você mapeou no docker-compose)
+    port: 5173,
+    // Garante que o Hot Module Replacement (HMR) funcione corretamente através do Docker
+    watch: {
+      usePolling: true,
+    },
+  },
 })
